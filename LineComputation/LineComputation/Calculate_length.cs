06256541fa85ff551b0744bcf34ec2daef94f638
echo # <priyankapardeshi2312@gmail.com>
2021-05-25 18:57:34 +0530

    Taken Two points And calculate the length

---
+++ LineComputation/LineComputation/Calculate_length.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputation
+{
+    class Calculate_length
+    {
+        public void Calculate(int x1, int y1, int x2, int y2)
+        {
+            float length = 0;
+            length = (float)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("length iss=" + length);
+        }
+    }
+}
--- LineComputation/LineComputation/Program.cs
+++ LineComputation/LineComputation/Program.cs
@@ -11,6 +11,8 @@ namespace LineComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Program to calculate the length the the line");
+            Calculate_length Obj = new Calculate_length();
+            Obj.Calculate(1,1,1,2);
             Console.ReadKey(); 
         }
     }
