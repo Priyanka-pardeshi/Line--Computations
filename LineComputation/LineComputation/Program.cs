822f7fd93994a8ead54b6c1f0afcf7bc68dfe666
echo # <priyankapardeshi2312@gmail.com>
2021-05-25 18:45:26 +0530

    Calculating the length

---
+++ LineComputation/LineComputation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to calculate the length the the line");
+            Console.ReadKey(); 
+        }
+    }
+}
