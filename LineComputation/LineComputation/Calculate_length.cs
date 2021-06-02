c8959a433452b2200773f1b5bdbf0db66ad40717
echo # <priyankapardeshi2312@gmail.com>
2021-06-02 22:57:02 +0530

    Added an OOPS concepts to line Computations

--- LineComputation/LineComputation/Calculate_length.cs
+++ LineComputation/LineComputation/Calculate_length.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 
 namespace LineComputation
 {
-    class Calculate_length
+    public abstract class Cal
     {
-        public float Calculate(int x1, int y1, int x2, int y2)
-        {
-            float length = 0;
-            length = (float)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("length iss=" + length);
-            return length;
-        }
+        public abstract float Call(int x1,int y1,int x2,int y2);
+    }
+    public class Calculate_length:Cal
+    {
+        public override float Call(int x1, int y1, int x2, int y2)
+    {
+        float length = 0;
+        length = (float)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+        Console.WriteLine("length iss=" + length);
+        return length;
+    }
+        
     }
 }
--- LineComputation/LineComputation/Program.cs
+++ LineComputation/LineComputation/Program.cs
@@ -11,31 +11,21 @@ namespace LineComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Program to calculate the length the the line");
-            Calculate_length Obj = new Calculate_length();
-            float Line1 = Obj.Calculate(1,1,1,2);
-            float Line2 = Obj.Calculate(1,2,2,2);
-            
+            Calculate cal;
+            cal = new Calculate_length();
+            float Line1=cal.Call(1, 1, 1, 2);
+            float Line2 = cal.Call(1, 2, 2, 2);
             //Equals
-            Boolean Check_equality=Line1.Equals(Line2);
-            if (Check_equality == true)
-            {
-                Console.WriteLine("Lines are Equal in length");
-            }
+            if (Line1.Equals(Line2) == true)         
+                Console.WriteLine("Lines are Equal in length");          
             else
-            {
                 Console.WriteLine("Lines are Not Equal");
-            }
 
             //compareTo
-            float Compare = Line1.CompareTo(Line2);
-            if (Compare == 0)
-            {
+            if (Line1.CompareTo(Line2) == 0) 
                 Console.WriteLine("Both Line are  Same");
-            }
             else
-            {
                 Console.WriteLine("They are not Same");
-            }
             Console.ReadKey(); 
         }
     }
